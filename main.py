117c086389ab7e344b5221fd9b05d4baeb21ad3a
ZychDev <szymonzychdev@gmail.com>
2021-06-12 11:05:43 +0200

    recognizes

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ train_images = train_images/255.0
 test_images = test_images/255.0
 
 #show data img in matplot
-plt.imshow(train_images[0], cmap=plt.cm.binary)
-plt.show()
+#plt.imshow(train_images[0], cmap=plt.cm.binary)
+#plt.show()
 
 
 #create model
@@ -30,9 +30,18 @@ model = keras.Sequential([
 #model compile options
 model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])#train model
 #epoch = how many times the model gonan see information
-model.fit(train_images, train_labels, epochs=10)
+model.fit(train_images, train_labels, epochs=5)
 
 #test model
-test_loss, test_acc = model.evaluate(test_images, test_labels)
+#test_loss, test_acc = model.evaluate(test_images, test_labels)
 
-print('\nTest accuracy:', test_acc)
+prediction = model.predict(test_images)
+
+
+
+for i in range(5):
+	plt.grid(False)
+	plt.imshow(test_images[i], cmap=plt.cm.binary)
+	plt.xlabel("Actual: " + class_names[test_labels[i]])
+	plt.title("Prediction: " + class_names[np.argmax(prediction[i])])
+	plt.show()
